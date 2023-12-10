682a5a6a447c41d9f7381c53aa5a83630dbf14fb
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2023-12-10 14:26:06 +0800

    Add SQLite database and routes for RSVP functionality

--- index.js
+++ index.js
@@ -1,12 +1,90 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+const port = 3000;
+
+// Initialize SQLite Database
+const dbPath = path.resolve(__dirname, "rsvp.db");
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run(
+      `CREATE TABLE IF NOT EXISTS rsvp (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            adults integer, 
+            children integer
+        )`,
+      (err) => {
+        if (err) {
+          // Table already created
+        } else {
+          console.log("Table just created");
+        }
+      }
+    );
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/submit", (req, res) => {
+  const { name, adults, children } = req.body;
+  const insert = "INSERT INTO rsvp (name, adults, children) VALUES (?,?,?)";
+  db.run(insert, [name, adults, children], (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    db.all("SELECT * FROM rsvp", [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      let totalAttendees = rows.reduce((acc, row) => acc + row.adults + row.children, 0);
+      let responseHtml = rows.map((row) => `<p>${row.name} - Adults: ${row.adults}, Children: ${row.children}</p>`).join("");
+      responseHtml += `<p>Total Attendees: ${totalAttendees}</p>`;
+      res.send(responseHtml);
+    });
+  });
+});
+
+// Add this new route to your server.js
+
+app.get("/rsvps", (req, res) => {
+  db.all("SELECT * FROM rsvp", [], (err, rows) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+
+    let totalAttendees = rows.reduce((acc, row) => acc + row.adults + row.children, 0);
+    let responseHtml = rows.map((row) => `<p>${row.name} - Adults: ${row.adults}, Children: ${row.children}</p>`).join("");
+    responseHtml += `<p>Total Attendees: ${totalAttendees}</p>`;
+    res.send(responseHtml);
+  });
+});
+
+app.post("/clear", (req, res) => {
+  db.run("DELETE FROM rsvp", [], (err) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    }
+    res.send("Cleared");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
