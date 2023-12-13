f7341b3772c8c50dab3191ba7a5821c4c3839d43
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2023-12-13 15:51:00 +0800

    Fix RSVP table generation and update fetchRSVPs function

--- index.js
+++ index.js
@@ -35,8 +35,34 @@ const db = new sqlite3.Database(dbPath, (err) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+// Function to generate the table
+const generateTable = (res) => {
+  db.all("SELECT * FROM rsvp", [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+
+    let totalAdults = rows.reduce((acc, row) => acc + row.adults, 0);
+    let totalChildren = rows.reduce((acc, row) => acc + row.children, 0);
+    let totalAttendees = totalAdults + totalChildren;
+
+    let tableHtml = `<table><tr><th>Name</th><th>Adults</th><th>Children</th></tr>`;
+    rows.forEach((row) => {
+      tableHtml += `<tr><td>${row.name}</td><td>${row.adults}</td><td>${row.children}</td></tr>`;
+    });
+    tableHtml += `<tr><td><strong>Totals</strong></td><td>${totalAdults}</td><td>${totalChildren}</td></tr>`;
+    tableHtml += `</table><p>Total Attendees: ${totalAttendees}</p>`;
+
+    res.send(tableHtml);
+  });
+};
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/rsvps", (req, res) => {
+  generateTable(res);
 });
 
 app.post("/submit", (req, res) => {
@@ -46,42 +72,55 @@ app.post("/submit", (req, res) => {
     if (err) {
       return console.error(err.message);
     }
-    db.all("SELECT * FROM rsvp", [], (err, rows) => {
-      if (err) {
-        throw err;
-      }
-
-      let totalAdults = rows.reduce((acc, row) => acc + row.adults, 0);
-      let totalChildren = rows.reduce((acc, row) => acc + row.children, 0);
-      let totalAttendees = totalAdults + totalChildren;
-
-      let tableHtml = `<table><tr><th>Name</th><th>Adults</th><th>Children</th></tr>`;
-      rows.forEach((row) => {
-        tableHtml += `<tr><td>${row.name}</td><td>${row.adults}</td><td>${row.children}</td></tr>`;
-      });
-      tableHtml += `<tr><td><strong>Totals</strong></td><td>${totalAdults}</td><td>${totalChildren}</td></tr>`;
-      tableHtml += `</table><p>Total Attendees: ${totalAttendees}</p>`;
-
-      res.send(tableHtml);
-    });
+    generateTable(res);
   });
 });
 
-// Add this new route to your server.js
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
-app.get("/rsvps", (req, res) => {
-  db.all("SELECT * FROM rsvp", [], (err, rows) => {
-    if (err) {
-      res.status(500).send(err.message);
-      return;
-    }
+// app.post("/submit", (req, res) => {
+//   const { name, adults, children } = req.body;
+//   const insert = "INSERT INTO rsvp (name, adults, children) VALUES (?,?,?)";
+//   db.run(insert, [name, adults, children], (err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     db.all("SELECT * FROM rsvp", [], (err, rows) => {
+//       if (err) {
+//         throw err;
+//       }
 
-    let totalAttendees = rows.reduce((acc, row) => acc + row.adults + row.children, 0);
-    let responseHtml = rows.map((row) => `<p>${row.name} - Adults: ${row.adults}, Children: ${row.children}</p>`).join("");
-    responseHtml += `<p>Total Attendees: ${totalAttendees}</p>`;
-    res.send(responseHtml);
-  });
-});
+//       let totalAdults = rows.reduce((acc, row) => acc + row.adults, 0);
+//       let totalChildren = rows.reduce((acc, row) => acc + row.children, 0);
+//       let totalAttendees = totalAdults + totalChildren;
+
+//       let tableHtml = `<table><tr><th>Name</th><th>Adults</th><th>Children</th></tr>`;
+//       rows.forEach((row) => {
+//         tableHtml += `<tr><td>${row.name}</td><td>${row.adults}</td><td>${row.children}</td></tr>`;
+//       });
+//       tableHtml += `<tr><td><strong>Totals</strong></td><td>${totalAdults}</td><td>${totalChildren}</td></tr>`;
+//       tableHtml += `</table><p>Total Attendees: ${totalAttendees}</p>`;
+
+//       res.send(tableHtml);
+//     });
+//   });
+// });
+
+// app.get("/rsvps", (req, res) => {
+//   db.all("SELECT * FROM rsvp", [], (err, rows) => {
+//     if (err) {
+//       res.status(500).send(err.message);
+//       return;
+//     }
+
+//     let totalAttendees = rows.reduce((acc, row) => acc + row.adults + row.children, 0);
+//     let responseHtml = rows.map((row) => `<p>${row.name} - Adults: ${row.adults}, Children: ${row.children}</p>`).join("");
+//     responseHtml += `<p>Total Attendees: ${totalAttendees}</p>`;
+//     res.send(responseHtml);
+//   });
+// });
 
 app.post("/clear", (req, res) => {
   db.run("DELETE FROM rsvp", [], (err) => {
